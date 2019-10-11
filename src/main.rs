15637cffd35bca659d359d0782580c5e32406686
Adam Szalkowski <adam@szalkowski.de>
2019-10-11 14:16:56 +0200

    use fancy 4D transformation matrix

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,15 @@ fn main() {
 
     #[derive(Copy, Clone)]
     struct Vertex {
-        position: [f32; 2],
+        position: [f32; 3],
         color: [f32; 3],
     }
     glium::implement_vertex!(Vertex, position, color);
 
     impl Vertex {
-        fn new(x: f32, y: f32) -> Vertex {
+        fn new(x: f32, y: f32, z: f32) -> Vertex {
             Vertex {
-                position: [x, y],
+                position: [x, y, z],
                 color: [0., 0., 0.],
             }
         }
@@ -35,9 +35,9 @@ fn main() {
     }
 
     let shape = vec![
-        Vertex::new(-0.5, -0.5).color(1.0, 0.0, 0.0),
-        Vertex::new(0.0, 0.5).color(0.0, 1.0, 0.0),
-        Vertex::new(0.5, -0.25).color(0.0, 0.0, 1.0),
+        Vertex::new(-0.5, -0.5, 0.0).color(1.0, 0.0, 0.0),
+        Vertex::new(0.0, 0.5, 0.0).color(0.0, 1.0, 0.0),
+        Vertex::new(0.5, -0.25, 0.0).color(0.0, 0.0, 1.0),
     ];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -57,6 +57,15 @@ fn main() {
 
     while running {
         let t = time.elapsed().unwrap().as_millis() as f32 / 1000.0; // precision reduced after 4.6h
+        let theta = (0.1 * t) % 1.0 * 2. * std::f32::consts::PI;
+        let sigma = (0.4 * t) % 1.0 * 2. * std::f32::consts::PI + 2.;
+        let transform = [
+            [theta.cos(), theta.sin(), 0.0, 0.0],
+            [-theta.sin(), theta.cos() * sigma.cos(), sigma.sin(), 0.0],
+            [0.0, -sigma.sin(), sigma.cos(), 0.0],
+            [0.0, 0.0, 0.0, 1.0f32],
+        ];
+
         let mut frame = display.draw();
         frame.clear_color(0.0, 0.0, 0.0, 1.0);
         frame
@@ -64,7 +73,7 @@ fn main() {
                 &vertex_buffer,
                 &indices,
                 &program,
-                &glium::uniform! {theta : (0.1 * t) % 1.0 * 2. * std::f32::consts::PI},
+                &glium::uniform! {transform : transform},
                 &Default::default(),
             )
             .unwrap();
--- src/triangle.vert
+++ src/triangle.vert
@@ -1,20 +1,12 @@
 #version 330 core
 
-uniform float theta;
-in vec2 position;
+uniform mat4 transform;
+in vec3 position;
 in vec3 color;
 out vec3 _color;
 
-vec2 rotate(vec2 position, float theta) {
-    return vec2(
-        position.x * cos(theta) - position.y * sin(theta),
-        position.x * sin(theta) + position.y * cos(theta)
-    );
-}
-
 void main()
 {
-    vec2 rotated = rotate(position, theta);
-    gl_Position = vec4(rotated, 0.0, 1.0);
+    gl_Position = transform * vec4(position, 1.0);
     _color = color;
 }
